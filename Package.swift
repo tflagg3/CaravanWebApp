57d0a83c5618dc2240d5565cd0057fa064689622
tflagg3 <t_flagg@coloradocollege.edu>
2025-02-11 12:14:34 -0700

    commented out apns package dependencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
-        .package(url: "hhtps://github.com/vapor/apns.git", from: "4.0.0")
+        // .package(url: "hhtps://github.com/vapor/apns.git", from: "4.0.0")
     ],
     targets: [
         .executableTarget(
@@ -20,7 +20,7 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
-                .product(name: "VaporAPNS", package: "apns")
+                // .product(name: "VaporAPNS", package: "apns")
             ],
             swiftSettings: swiftSettings
         ),
